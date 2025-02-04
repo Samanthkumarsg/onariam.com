a995237ffe38a09acfcbf9db5f9e9fc95cb68633
samanth kumar <samanthkumarsg@gmail.com>
2025-02-04 22:42:52 +0100

    Typography Changes

--- src/app/components/Services/Card.jsx
+++ src/app/components/Services/Card.jsx
@@ -8,8 +8,8 @@ export const Card = ({ icon, className, hasChildrenPills, title, description }) 
             <div className='mb-2 flex items-start justify-start'>
                 {icon}
             </div>
-            <div className='flex flex-col'>
-                <h1 className='text-xl text-secondary font-semibold mb-4 uppercase'>{title}</h1>
+            <div className='flex flex-col mt-2'>
+                <h1 className='text-lg text-secondary font-semibold mb-1 uppercase'>{title}</h1>
                 <p className='text-base leading-normal text-neutral-700 tracking-wide '>{description}</p>
             </div>
             {hasChildrenPills && (<div className='mt-7 flex flex-col items-start justify-start gap-4 w-full *:w-full *:bg-orange-50 *:text-orange-700 font-medium *:tracking-wider *:p-2 *:text-sm *:rounded-lg *:border *:border-orange-700'>
